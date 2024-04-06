743a83a775ec952ed08af23c121a820df7724eeb
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-06 22:50:11 +0300

    commit_add_TestimonialController_Consume

---
+++ Frontend/HotelProject.WebUI/Models/Testimonial/CreateTestimonialViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelProject.WebUI.Models.Testimonial
+{
+    public class CreateTestimonialViewModel
+    {
+        public string TestimonialName { get; set; }
+
+        public string TestimonialTitle { get; set; }
+
+        public string TestimonialDescription { get; set; }
+
+        public string TestimonialImage { get; set; }
+    }
+}
