f4ca81fddd4374a524298ee34aa851295041de63
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-10 02:20:31 +0300

    commit_add_JWT

---
+++ JWTProject/WebApiJWT/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJWT.Models;
+
+namespace WebApiJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ActionResult Test() 
+        {
+            return Ok(new CreateToken().GenerateToken());
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult Test1()
+        {
+            return Ok(new CreateToken().GenerateTokenToAdmin());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin, Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı bir şekilde giriş yaptı");
+        }
+    }
+}
---
+++ JWTProject/WebApiJWT/Models/CreateToken.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiJWT.Models
+{
+    public class CreateToken
+    {
+        public string GenerateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi..//1234567api///api");
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(5), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(token);
+        }
+
+        public string GenerateTokenToAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi..//1234567api///api");
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim (ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim (ClaimTypes.Role, "Admin"),
+                new Claim (ClaimTypes.Role, "Visitor")
+            };
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", claims: claims, notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(5), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(token);
+        }
+    }
+}
